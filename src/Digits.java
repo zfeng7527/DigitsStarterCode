ce34fe58305434b2673443226f91176fc1a8a3ef
natefolwell <nfolwell@bths.edu>
2019-04-04 08:41:07 -0400

    Initial commit

---
+++ src/Digits.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+
+public class Digits
+{
+    //The list of digits from the number used to //construct this object
+    ArrayList<Integer> digitList;
+
+    //Constructs a Digits object that
+    //represents num
+    public Digits (int num)
+    {
+        //implemented in part (a)
+    }
+    public boolean isStrictlyIncreasing()
+    {
+        //implemented in part (b)
+    }
+    //Extra method to make the runner easier to read
+    public ArrayList<Integer> getDigitList()
+    {
+        return digitList;
+    }
+
+}
---
+++ src/DigitsRunner.java
@@ -0,0 +1,10 @@
+public class DigitsRunner
+{
+    public static void main(String[] args)
+    {
+        Digits d = new Digits(8675309);
+        System.out.println(d.getDigitList());
+        Digits d0 = new Digits(0);
+        System.out.println(d.getDigitsList());
+    }
+}
